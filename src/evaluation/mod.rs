62569f9e98311fa6a5d0218c74e3fc6ef07658d2
Bruno Mendes <bd_mendes@outlook.com>
2023-02-11 02:03:43 +0000

    Refine pieces values

--- src/evaluation/mod.rs
+++ src/evaluation/mod.rs
@@ -16,8 +16,8 @@ pub fn piece_value(piece: Piece) -> Score {
     match piece {
         Piece::WP | Piece::BP => 100,
         Piece::WN | Piece::BN => 310,
-        Piece::WB | Piece::BB => 330,
-        Piece::WR | Piece::BR => 480,
+        Piece::WB | Piece::BB => 320,
+        Piece::WR | Piece::BR => 500,
         Piece::WQ | Piece::BQ => 900,
         _ => 0,
     }
