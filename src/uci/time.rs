fad8a209f4ebe97d6541b41cd2aad043ac0bbda9
Bruno Mendes <bd_mendes@outlook.com>
2023-02-11 01:56:15 +0000

    Play faster when low on time

--- src/uci/time.rs
+++ src/uci/time.rs
@@ -3,16 +3,16 @@ use std::time::Duration;
 use crate::position::{Color, Position};
 
 fn expected_remaining_moves(position: &Position) -> u32 {
-    const TYPICAL_MOVES_PER_GAME: u16 = 50;
-    if position.full_move_number > TYPICAL_MOVES_PER_GAME - 10 {
-        return 10;
+    const TYPICAL_MOVES_PER_GAME: u16 = 60;
+    if position.full_move_number > TYPICAL_MOVES_PER_GAME - 20 {
+        return 20;
     }
     (TYPICAL_MOVES_PER_GAME - position.full_move_number) as u32
 }
 
 fn get_duration_based_on_eval(position: &Position, time: Duration) -> Duration {
     let number_available_moves = position.legal_moves(false).len() + 1;
-    let cof = 10 + (std::cmp::min(number_available_moves / 3, 10)) as u32;
+    let cof = 10 + (std::cmp::min(number_available_moves / 4, 10)) as u32;
     let expected_remaining = expected_remaining_moves(position);
     cof * time / (expected_remaining * 20)
 }
@@ -33,13 +33,17 @@ pub fn get_duration(
         Color::Black => black_increment,
     };
 
-    let standard_move_time = get_duration_based_on_eval(position, our_duration);
+    let standard_move_time = if our_duration < Duration::from_millis(500) {
+        Duration::from_millis(10)
+    } else {
+        get_duration_based_on_eval(position, our_duration)
+    };
 
     if let Some(our_increment) = our_increment {
-        if our_increment > Duration::from_millis(100) {
+        if our_increment > Duration::from_millis(200) {
             let new_move_time = standard_move_time + our_increment;
             if new_move_time < our_duration {
-                return new_move_time - Duration::from_millis(100);
+                return new_move_time - Duration::from_millis(200);
             }
         }
     }
