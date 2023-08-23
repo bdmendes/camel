0b8548b56b16f6a46a49b78aa2bdf4b4555cb2d0
Bruno Mendes <bd_mendes@outlook.com>
2023-08-23 17:52:24 +0100

    Add bash formatter job

--- gauntlet.sh
+++ gauntlet.sh
@@ -13,17 +13,14 @@ readonly THREADS=4
 readonly ELO_THRESHOLD=20
 readonly UPSTREAM=${1:-master} # Default to master if no argument is given
 
-if ! git diff --quiet
-then
+if ! git diff --quiet; then
     echo "Commit or stash your changes first"
     exit 1
 fi
 
-if [ -z "$CURRENT_BRANCH" ]
-then
+if [ -z "$CURRENT_BRANCH" ]; then
     CURRENT_BRANCH=$(git branch --show-current)
-    if [ -z "$CURRENT_BRANCH" ]
-    then
+    if [ -z "$CURRENT_BRANCH" ]; then
         echo "Could not get current branch name (are you in a detached state?)"
         exit 1
     fi
@@ -33,11 +30,10 @@ else
 fi
 
 # Clone fast-chess and compile it if not found
-if ! command -v $INSTALL_PATH/$RUNNER &> /dev/null
-then
+if ! command -v $INSTALL_PATH/$RUNNER &>/dev/null; then
     echo "$RUNNER not found, cloning and compiling"
     rm -rf $INSTALL_PATH
-    git clone $REPO_URL --branch $REPO_TAG --single-branch $INSTALL_PATH &> /dev/null || exit 1
+    git clone $REPO_URL --branch $REPO_TAG --single-branch $INSTALL_PATH &>/dev/null || exit 1
     make -C $INSTALL_PATH || exit 1
 fi
 
@@ -54,8 +50,7 @@ cp $BUILD_PATH "$INSTALL_PATH/${ENGINE_NAME}-$UPSTREAM" || exit 1
 git switch "$CURRENT_BRANCH" || exit 1
 cd $INSTALL_PATH || exit 1
 
-if cmp -s "${ENGINE_NAME}-${CURRENT_BRANCH}" "${ENGINE_NAME}-$UPSTREAM"
-then
+if cmp -s "${ENGINE_NAME}-${CURRENT_BRANCH}" "${ENGINE_NAME}-$UPSTREAM"; then
     echo -n "🆗 Engine executables do not differ: gauntlet skipped" | tee $MESSAGE_FILE
     echo ""
     exit
@@ -64,9 +59,9 @@ fi
 # Run the gauntlet and store output in temp file
 OUTPUT_FILE=$(mktemp)
 stdbuf -i0 -o0 -e0 ./${RUNNER} \
--engine cmd=${ENGINE_NAME}-"${CURRENT_BRANCH}" name=${ENGINE_NAME}-"${CURRENT_BRANCH}" \
--engine cmd=${ENGINE_NAME}-"$UPSTREAM" name=${ENGINE_NAME}-"$UPSTREAM" \
--each tc=$TIME_CONTROL -rounds $ROUNDS -repeat -concurrency $THREADS | tee "$OUTPUT_FILE"
+    -engine cmd=${ENGINE_NAME}-"${CURRENT_BRANCH}" name=${ENGINE_NAME}-"${CURRENT_BRANCH}" \
+    -engine cmd=${ENGINE_NAME}-"$UPSTREAM" name=${ENGINE_NAME}-"$UPSTREAM" \
+    -each tc=$TIME_CONTROL -rounds $ROUNDS -repeat -concurrency $THREADS | tee "$OUTPUT_FILE"
 
 # Print last evaluation result
 result=$(grep +/- "$OUTPUT_FILE" | tail -1)
@@ -74,12 +69,10 @@ result_array=("$result")
 elo_diff=${result_array[2]}
 elo_diff_rounded=$(echo "$elo_diff" | awk '{printf("%d\n",$1 + 0.5)}')
 failed=0
-if [ $((elo_diff_rounded)) -lt -$ELO_THRESHOLD ]
-then
+if [ $((elo_diff_rounded)) -lt -$ELO_THRESHOLD ]; then
     echo -n "❌ " | tee $MESSAGE_FILE
     failed=1
-elif [ $((elo_diff_rounded)) -lt $ELO_THRESHOLD ]
-then
+elif [ $((elo_diff_rounded)) -lt $ELO_THRESHOLD ]; then
     echo -n "🆗 " | tee $MESSAGE_FILE
     exit
 else
@@ -88,7 +81,6 @@ fi
 echo -n "$result" | tee -a $MESSAGE_FILE
 echo ""
 
-if [ $failed == 1 ]
-then
+if [ $failed == 1 ]; then
     exit 1
 fi
