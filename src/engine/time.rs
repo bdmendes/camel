5abdd3c8aee34cdac62a0a4a48cd31ebc803eb44
Bruno Mendes <bd_mendes@outlook.com>
2023-08-27 18:51:30 +0100

    Spend more time after the opening

--- src/engine/time.rs
+++ src/engine/time.rs
@@ -8,7 +8,8 @@ fn get_duration_based_on_moves(position: &Position, time: Duration) -> Duration 
         std::cmp::max(10, TYPICAL_GAME_MOVES.saturating_sub(position.fullmove_number));
     let regular_time = time / expected_remaining_moves as u32;
 
-    let parabole_function = |x: f32| 0.01 * (150.0 - (x - 20.0) * (x - 20.0));
+    // Spend more time after the opening
+    let parabole_function = |x: f32| 0.01 * (250.0 - (x - 20.0) * (x - 20.0));
     let parabole_factor = parabole_function(position.fullmove_number as f32);
 
     regular_time.mul_f32(parabole_factor.max(0.8))
@@ -33,7 +34,7 @@ pub fn get_duration(
     let mut standard_move_time = get_duration_based_on_moves(position, our_duration);
 
     if standard_move_time < Duration::from_secs(1) {
-        standard_move_time /= 2;
+        standard_move_time /= 3;
     }
 
     if let Some(our_increment) = our_increment {
