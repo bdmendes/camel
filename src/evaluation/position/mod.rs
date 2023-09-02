215f1214ffea1ee0c50cb4b663e6c579912ae36c
Bruno Mendes <bd_mendes@outlook.com>
2023-09-02 16:29:50 +0100

    Remove mobility score

--- src/evaluation/position/mod.rs
+++ src/evaluation/position/mod.rs
@@ -1,83 +1,54 @@
-use self::{pawns::evaluate_pawn_structure, psqt::psqt_value};
+use self::pawns::evaluate_pawn_structure;
 
-use super::{piece_value, ValueScore};
-use crate::{
-    moves::gen::piece_attacks,
-    position::{board::Piece, Color, Position},
-};
+use super::{piece_value, psqt::psqt_value, ValueScore};
+use crate::position::{board::Piece, Position};
 
 mod pawns;
-pub mod psqt;
 
 pub const MAX_POSITIONAL_GAIN: ValueScore = 200;
 
+pub fn piece_midgame_ratio(piece: Piece) -> u8 {
+    match piece {
+        Piece::Pawn => 0,
+        Piece::Knight => 10,
+        Piece::Bishop => 10,
+        Piece::Rook => 16,
+        Piece::Queen => 32,
+        Piece::King => 0,
+    }
+}
+
 pub fn midgame_ratio(position: &Position) -> u8 {
-    Piece::list().iter().fold(0, |acc, piece| {
-        acc.saturating_add(
-            position.board.pieces_bb(*piece).count_ones() as u8
-                * match *piece {
-                    Piece::Pawn => 4,
-                    Piece::Knight => 10,
-                    Piece::Bishop => 10,
-                    Piece::Rook => 16,
-                    Piece::Queen => 30,
-                    Piece::King => 0,
-                },
-        )
-    })
+    let mut ratio: u8 = 0;
+    for piece in Piece::list() {
+        ratio = ratio.saturating_add(
+            position.board.pieces_bb(*piece).count_ones() as u8 * piece_midgame_ratio(*piece),
+        );
+    }
+    ratio
 }
 
 pub fn evaluate_position(position: &Position) -> ValueScore {
     let endgame_ratio = 255 - midgame_ratio(position);
+    let mut score = 0;
 
-    let occupancy = position.board.occupancy_bb_all();
-    let white_occupancy = position.board.occupancy_bb(Color::White);
-    let black_occupancy = position.board.occupancy_bb(Color::Black);
+    for piece in Piece::list() {
+        let piece_value = piece_value(*piece);
 
-    let base_score = Piece::list().iter().fold(0, |acc, piece| {
-        let bb = position.board.pieces_bb(*piece);
-        let white_pieces = bb & white_occupancy;
-        let black_pieces = bb & black_occupancy;
+        for square in position.board.pieces_bb(*piece) {
+            let color = position.board.color_at(square).unwrap();
+            let color_sign = color.sign();
 
-        // Material score
-        let material_bonus = piece_value(*piece)
-            * (white_pieces.count_ones() as ValueScore - black_pieces.count_ones() as ValueScore);
+            // Material score
+            score += piece_value * color_sign;
 
-        // PSQT score
-        let white_positional_bonus = white_pieces
-            .into_iter()
-            .fold(0, |acc, square| acc + psqt_value(*piece, square, Color::White, endgame_ratio));
-        let black_positional_bonus = black_pieces
-            .into_iter()
-            .fold(0, |acc, square| acc + psqt_value(*piece, square, Color::Black, endgame_ratio));
-        let positional_bonus = white_positional_bonus - black_positional_bonus;
+            // Positional score
+            let psqt_score = psqt_value(*piece, square, color, endgame_ratio);
+            score += psqt_score * color_sign;
+        }
+    }
 
-        // Mobility score
-        let mobility_bonus = if *piece == Piece::Pawn {
-            0
-        } else {
-            let piece_mobility_bonus = match *piece {
-                Piece::Bishop => 4,
-                Piece::Rook | Piece::Knight => 3,
-                Piece::Queen => 2,
-                Piece::King => -1,
-                _ => unreachable!(),
-            };
-            let white_mobility_bonus = white_pieces.into_iter().fold(0, |acc, square| {
-                let attacks = piece_attacks(*piece, square, occupancy) & !white_occupancy;
-                acc + attacks.count_ones() as ValueScore * piece_mobility_bonus
-            });
-            let black_mobility_bonus = black_pieces.into_iter().fold(0, |acc, square| {
-                let attacks = piece_attacks(*piece, square, occupancy) & !black_occupancy;
-                acc + attacks.count_ones() as ValueScore * piece_mobility_bonus
-            });
-            white_mobility_bonus - black_mobility_bonus
-        };
-
-        acc + material_bonus + positional_bonus + mobility_bonus
-    });
-
-    base_score + evaluate_pawn_structure(position)
+    score + evaluate_pawn_structure(position)
 }
 
 #[cfg(test)]
