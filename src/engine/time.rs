2afbfd166385788d288a5f129a4fdfc8921b6dce
Bruno Mendes <bd_mendes@outlook.com>
2023-09-17 14:32:48 +0100

    Tweak time formula

--- src/engine/time.rs
+++ src/engine/time.rs
@@ -1,14 +1,14 @@
 use camel::position::{Color, Position};
 use std::time::Duration;
 
-const TYPICAL_GAME_MOVES: u16 = 50;
+const TYPICAL_GAME_MOVES: u16 = 60;
 
 fn get_duration_based_on_moves(position: &Position, time: Duration) -> Duration {
     let expected_remaining_moves =
         std::cmp::max(10, TYPICAL_GAME_MOVES.saturating_sub(position.fullmove_number));
     let regular_time = time / expected_remaining_moves as u32;
 
-    let parabole_function = |x: f32| 0.01 * (150.0 - (x - 20.0) * (x - 20.0));
+    let parabole_function = |x: f32| 0.01 * (200.0 - (x - 20.0) * (x - 20.0));
     let parabole_factor = parabole_function(position.fullmove_number as f32);
 
     regular_time.mul_f32(parabole_factor.max(0.8))
@@ -32,8 +32,8 @@ pub fn get_duration(
 
     let mut standard_move_time = get_duration_based_on_moves(position, our_duration);
 
-    if standard_move_time < Duration::from_secs(1) {
-        standard_move_time /= 2;
+    if standard_move_time < Duration::from_millis(500) {
+        standard_move_time /= 4;
     }
 
     if let Some(our_increment) = our_increment {
