49cca65a2041d7f31b95d46b430f114f15df3be8
Bruno Mendes <bd_mendes@outlook.com>
2023-10-25 12:06:39 +0100

    Integrate move stage into movegen

--- benches/movegen.rs
+++ benches/movegen.rs
@@ -1,20 +1,23 @@
-use camel::position::{
-    fen::{KIWIPETE_BLACK_FEN, KIWIPETE_WHITE_FEN},
-    Position,
+use camel::{
+    moves::gen::MoveStage,
+    position::{
+        fen::{KIWIPETE_BLACK_FEN, KIWIPETE_WHITE_FEN},
+        Position,
+    },
 };
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn generate_moves_kiwipete_white(c: &mut Criterion) {
     let kiwipete_position_white = Position::from_fen(KIWIPETE_WHITE_FEN).unwrap();
     c.bench_function("generate_moves_kiwipete_white", |b| {
-        b.iter(|| kiwipete_position_white.moves(false));
+        b.iter(|| kiwipete_position_white.moves(MoveStage::All));
     });
 }
 
 fn generate_moves_kiwipete_black(c: &mut Criterion) {
     let kiwipete_position_black = Position::from_fen(KIWIPETE_BLACK_FEN).unwrap();
     c.bench_function("generate_moves_kiwipete_black", |b| {
-        b.iter(|| kiwipete_position_black.moves(false));
+        b.iter(|| kiwipete_position_black.moves(MoveStage::All));
     });
 }
 
