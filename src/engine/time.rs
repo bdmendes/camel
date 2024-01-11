369e5f693263b9e7a6c21016f3fa7c8b59d37496
Bruno Mendes <bd_mendes@outlook.com>
2024-01-11 21:27:00 +0000

    Spend more time when one has more moves

--- src/engine/time.rs
+++ src/engine/time.rs
@@ -1,4 +1,7 @@
-use camel::position::{Color, Position};
+use camel::{
+    moves::gen::MoveStage,
+    position::{Color, Position},
+};
 use std::time::Duration;
 
 const TYPICAL_GAME_MOVES: u16 = 50;
@@ -8,7 +11,7 @@ fn get_duration_based_on_moves(position: &Position, time: Duration) -> Duration 
         std::cmp::max(10, TYPICAL_GAME_MOVES.saturating_sub(position.fullmove_number));
     let regular_time = time / expected_remaining_moves as u32;
 
-    let parabole_function = |x: f32| 0.01 * (150.0 - (x - 20.0) * (x - 20.0));
+    let parabole_function = |x: f32| 0.01 * (200.0 - (x - 20.0) * (x - 20.0));
     let parabole_factor = parabole_function(position.fullmove_number as f32);
 
     regular_time.mul_f32(parabole_factor.max(0.8))
@@ -30,7 +33,10 @@ pub fn get_duration(
         Color::Black => black_increment,
     };
 
-    let mut standard_move_time = get_duration_based_on_moves(position, our_duration);
+    let moves_cof = position.moves(MoveStage::All).len() as f32 / 40.0;
+    let mut standard_move_time = get_duration_based_on_moves(position, our_duration)
+        .mul_f32(moves_cof)
+        .max(Duration::from_millis(10));
 
     if standard_move_time < Duration::from_secs(1) {
         standard_move_time /= 2;
