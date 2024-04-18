cc5e3f7a641048c57a9ba734f58e30e396ad04ce
Bruno Mendes <bd_mendes@outlook.com>
2024-04-18 18:29:01 +0100

    Use number of pieces as time heuristic

--- src/engine/time.rs
+++ src/engine/time.rs
@@ -1,17 +1,25 @@
 use camel::position::{Color, Position};
-use std::time::Duration;
+use std::{ops::Div, time::Duration};
 
-const TYPICAL_GAME_MOVES: u16 = 50;
+const TYPICAL_GAME_MOVES: u16 = 40;
+const AVERAGE_NUMBER_OF_PIECES: u16 = 24;
 
 fn get_duration_based_on_moves(position: &Position, time: Duration) -> Duration {
+    // Assume the game will have a typical duration and divide the time by the expected remaining moves.
     let expected_remaining_moves =
         std::cmp::max(10, TYPICAL_GAME_MOVES.saturating_sub(position.fullmove_number));
     let regular_time = time / expected_remaining_moves as u32;
 
-    let parabole_function = |x: f32| 0.01 * (150.0 - (x - 20.0) * (x - 20.0));
-    let parabole_factor = parabole_function(position.fullmove_number as f32);
+    // Positions tend to get more tense around move 20, so we want to increase the time around that move.
+    let parabole_function = |x: f32| 0.01 * (200.0 - (x - 20.0) * (x - 20.0));
+    let move_number_factor = parabole_function(position.fullmove_number as f32).max(0.8);
 
-    regular_time.mul_f32(parabole_factor.max(0.8))
+    // Positions with less space tend to be more complex, so increase the time for those.
+    let number_of_pieces = position.board.occupancy_bb_all().count_ones() as f32;
+    let number_of_pieces_factor =
+        number_of_pieces.div(AVERAGE_NUMBER_OF_PIECES as f32).max(0.9).min(1.2);
+
+    regular_time.mul_f32(move_number_factor).mul_f32(number_of_pieces_factor)
 }
 
 pub fn get_duration(
