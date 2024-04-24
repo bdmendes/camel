d7df7e568e03a3babc4eb94903c9a4681d7d0384
Bruno Mendes <bd_mendes@outlook.com>
2024-04-24 08:02:42 +0100

    Use SEE for move ordering

--- src/evaluation/moves.rs
+++ src/evaluation/moves.rs
@@ -2,6 +2,7 @@ use super::{psqt::psqt_value, Evaluable, ValueScore};
 use crate::{
     moves::Move,
     position::{board::Piece, Position},
+    search::see,
 };
 
 pub fn evaluate_move(position: &Position, mov: Move) -> ValueScore {
@@ -10,8 +11,7 @@ pub fn evaluate_move(position: &Position, mov: Move) -> ValueScore {
     let moving_piece = position.board.piece_at(mov.from()).unwrap();
 
     if mov.flag().is_capture() {
-        let captured_piece = position.board.piece_at(mov.to()).unwrap_or(Piece::Pawn);
-        score += captured_piece.value() - moving_piece.value() + Piece::Queen.value();
+        score += see::see::<false>(mov, &position.board) + Piece::Pawn.value();
     }
 
     if let Some(promotion_piece) = mov.promotion_piece() {
@@ -22,27 +22,4 @@ pub fn evaluate_move(position: &Position, mov: Move) -> ValueScore {
     score -= psqt_value(moving_piece, mov.from(), position.side_to_move, 0);
 
     score
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        moves::gen::MoveStage,
-        position::{fen::FromFen, Position},
-    };
-
-    #[test]
-    fn eval_move_heuristic_value() {
-        let position = Position::from_fen(
-            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1",
-        )
-        .unwrap();
-        let mut moves = position.moves(MoveStage::All);
-        moves.sort_by(|a, b| {
-            super::evaluate_move(&position, *b).cmp(&super::evaluate_move(&position, *a))
-        });
-
-        let first_move = moves[0].to_string();
-        assert!(first_move == "e2a6" || first_move == "d5e6"); // equal trade of piece
-    }
 }
