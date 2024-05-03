5a069127eded621b2958864a1b1849f1cf3a9351
Bruno Mendes <bd_mendes@outlook.com>
2024-05-03 16:10:07 +0100

    Tune tropism and shelter parameters

--- src/evaluation/position/king.rs
+++ src/evaluation/position/king.rs
@@ -3,57 +3,55 @@ use crate::{
     position::{bitboard::Bitboard, board::Piece, square::Square, Color, Position},
 };
 
+const SHELTER_PENALTY: ValueScore = -25;
+
 fn king_pawn_shelter(position: &Position, king_color: Color, king_square: Square) -> ValueScore {
-    let mut shelter = 0;
-
+    let file_min = king_square.file().saturating_sub(1);
+    let file_max = king_square.file().min(6) + 1;
     let our_pawns = position.board.pieces_bb_color(Piece::Pawn, king_color);
+    let king_square_rank = king_square.rank() as i8;
 
-    let file_min = match king_square.file() {
-        0 => 0,
-        _ => king_square.file() - 1,
-    };
-    let file_max = match king_square.file() {
-        7 => 7,
-        _ => king_square.file() + 1,
-    };
-
-    const SHELTER_PENALTY: ValueScore = -20;
+    let mut shelter_penalty = 0;
 
     for file in file_min..=file_max {
         let our_pawns_on_file = our_pawns & Bitboard::file_mask(file);
-        let most_advanced_pawn = match king_color {
-            Color::White => our_pawns_on_file.into_iter().next(),
-            Color::Black => our_pawns_on_file.into_iter().next_back(),
-        };
-        if let Some(pawn_square) = most_advanced_pawn {
-            let rank_diff = (pawn_square.rank() as i8 - king_square.rank() as i8).abs();
-            let shelter_penalty = match rank_diff {
-                0 => 0,
-                1 => 0,
-                2 => SHELTER_PENALTY / 2,
-                3 => SHELTER_PENALTY,
-                _ => SHELTER_PENALTY * 2,
-            };
-            shelter += shelter_penalty;
-        } else {
-            shelter += SHELTER_PENALTY;
+
+        if our_pawns_on_file.is_empty() {
+            shelter_penalty += SHELTER_PENALTY * 2;
+            continue;
+        }
+
+        let most_advanced_pawn_rank_diff = (match king_color {
+            Color::White => {
+                our_pawns_on_file.into_iter().next_back().unwrap().rank() as i8 - king_square_rank
+            }
+            Color::Black => {
+                our_pawns_on_file.into_iter().next().unwrap().rank() as i8 - king_square_rank
+            }
+        })
+        .unsigned_abs();
+
+        shelter_penalty += match most_advanced_pawn_rank_diff {
+            0 | 1 => 0,
+            2 => SHELTER_PENALTY / 2,
+            3 | 4 => SHELTER_PENALTY,
+            _ => 0,
         }
     }
 
-    shelter
+    shelter_penalty
 }
 
 fn king_tropism(position: &Position, king_color: Color, king_square: Square) -> ValueScore {
     let them_occupancy = position.board.occupancy_bb(king_color.opposite())
-        & !position.board.pieces_bb(Piece::Pawn)
-        & !position.board.pieces_bb(Piece::King);
+        & !position.board.pieces_bb(Piece::King)
+        & !position.board.pieces_bb(Piece::Pawn);
 
     let tropism = them_occupancy.fold(0, |acc, sq| {
         let distance = sq.manhattan_distance(king_square);
         let piece_cof = match position.board.piece_at(sq) {
-            Some(Piece::Queen) | Some(Piece::Rook) => 2,
-            Some(Piece::Bishop) | Some(Piece::Knight) => 1,
-            _ => unreachable!(),
+            Some(Piece::Queen) => 2,
+            _ => 1,
         };
         acc + ((14 - distance) * piece_cof) as ValueScore
     });
@@ -63,38 +61,26 @@ fn king_tropism(position: &Position, king_color: Color, king_square: Square) -> 
 
 pub fn evaluate_king_safety(position: &Position, midgame_ratio: u8) -> ValueScore {
     let white_king_square =
-        position.board.pieces_bb_color(Piece::King, Color::White).into_iter().next();
+        position.board.pieces_bb_color(Piece::King, Color::White).into_iter().next().unwrap();
     let black_king_square =
-        position.board.pieces_bb_color(Piece::King, Color::Black).into_iter().next();
-
-    if white_king_square.is_none() || black_king_square.is_none() {
-        return 0;
-    }
+        position.board.pieces_bb_color(Piece::King, Color::Black).into_iter().next().unwrap();
 
     let mut score = 0;
 
-    score += king_tropism(position, Color::White, white_king_square.unwrap())
-        * midgame_ratio as ValueScore
-        / 255;
-    score -= king_tropism(position, Color::Black, black_king_square.unwrap())
-        * midgame_ratio as ValueScore
-        / 255;
+    score += king_tropism(position, Color::White, white_king_square);
+    score -= king_tropism(position, Color::Black, black_king_square);
 
-    score += king_pawn_shelter(position, Color::White, white_king_square.unwrap())
-        * midgame_ratio as ValueScore
-        / 255;
-    score -= king_pawn_shelter(position, Color::Black, black_king_square.unwrap())
-        * midgame_ratio as ValueScore
-        / 255;
+    score += king_pawn_shelter(position, Color::White, white_king_square);
+    score -= king_pawn_shelter(position, Color::Black, black_king_square);
 
-    score
+    score.saturating_mul(midgame_ratio as ValueScore) / 255
 }
 
 #[cfg(test)]
 mod tests {
     use super::king_tropism;
     use crate::{
-        evaluation::ValueScore,
+        evaluation::{position::king::SHELTER_PENALTY, ValueScore},
         position::{
             board::Piece,
             fen::{FromFen, START_FEN},
@@ -148,7 +134,7 @@ mod tests {
             Position::from_fen("r2q1rk1/1p2bppp/p2p4/3Ppb2/6P1/PN2BP2/1PP4P/R2Q1RK1 b - - 0 15")
                 .unwrap();
 
-        assert!((-40..=-20).contains(&position_shelter(&position)));
+        assert_eq!(position_shelter(&position), (SHELTER_PENALTY / 2 + SHELTER_PENALTY));
     }
 
     #[test]
@@ -157,7 +143,7 @@ mod tests {
             Position::from_fen("r4r1k/1p2p1pp/p2p2b1/3P4/6P1/PNP1q1P1/1P3R2/R2Q2K1 w - - 1 22")
                 .unwrap();
 
-        assert!((-120..=-50).contains(&position_shelter(&position)));
+        assert_eq!(position_shelter(&position), SHELTER_PENALTY * 4 + SHELTER_PENALTY);
     }
 
     #[test]
@@ -166,6 +152,6 @@ mod tests {
             Position::from_fen("r2q1rk1/1p2bppp/p2p4/3Ppb2/8/PN2BP2/1PP3PP/R2Q1RK1 w - - 1 15")
                 .unwrap();
 
-        assert!((-10..=-2).contains(&position_shelter(&position)));
+        assert_eq!(position_shelter(&position), SHELTER_PENALTY / 2);
     }
 }
