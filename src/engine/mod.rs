0723604513a05b1a19fa058cd9ca81f70dd11a3a
Bruno Mendes <bd_mendes@outlook.com>
2026-01-16 22:43:44 +0000

    Call search and have searcher store refs

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,18 +1,21 @@
 use std::{
     str::FromStr,
     sync::{Arc, Mutex},
+    time::Duration,
 };
 
 use crate::{
     evaluation::{
-        NNUE_PARAMS_BLOB,
+        NNUE_PARAMS_BLOB, ValueScore,
         nnue::{NeuralNetwork, Parameters},
     },
     position::{Position, fen::START_POSITION},
     search::{
+        NodeType, Searcher,
         game_history::GameHistory,
         score_table::{DEFAULT_TABLE_SIZE_MB, ScoreTable},
-        status::SearchStatus,
+        status::{SearchStatus, SearchStatusValue},
+        window::Window,
     },
 };
 
@@ -41,5 +44,33 @@ impl Default for Engine {
             game_history: Arc::new(Mutex::new(GameHistory::default())),
             search_status: SearchStatus::default(),
         }
+    }
+}
+
+impl Engine {
+    pub fn go(&self) {
+        let history = self.game_history.clone();
+        let table = self.score_table.clone();
+        let net = self.evaluator.clone();
+        let status = self.search_status.clone();
+        let position = self.position;
+
+        std::thread::spawn(move || {
+            let mut history = history.lock().unwrap();
+            let mut table = table.lock().unwrap();
+            let mut net = net.lock().unwrap();
+            let duration = Duration::from_hours(1);
+
+            let mut searcher = Searcher::new(&mut history, &mut table, &mut net, status.clone(), duration);
+
+            status.set(SearchStatusValue::Searching);
+            searcher.pvs(&position, 5, 0, NodeType::PVNode, Window::default());
+            status.set(SearchStatusValue::Stopped);
+            println!("move: {}", table.hash_move(&position).unwrap());
+            println!(
+                "eval: {}",
+                table.probe(&position, 5, 0).unwrap().0 * position.side_to_move().sign() as ValueScore
+            );
+        });
     }
 }
