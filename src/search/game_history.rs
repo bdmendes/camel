991c4a1a440f306110af21afdd5f60a86a70b335
Bruno Mendes <bd_mendes@outlook.com>
2026-01-21 20:32:52 +0000

    Fix sign and add back from_moves test

--- src/search/game_history.rs
+++ src/search/game_history.rs
@@ -8,7 +8,7 @@ pub struct GameHistory {
 
 impl GameHistory {
     pub fn new(position: &Position) -> Self {
-        let sign = position.side_to_move().sign() as usize;
+        let sign = position.side_to_move() as usize;
         let mut history = Self {
             hashes: [Vec::with_capacity(32), Vec::with_capacity(32)],
             barriers: [Vec::with_capacity(16), Vec::with_capacity(16)],
@@ -35,7 +35,7 @@ impl GameHistory {
     }
 
     pub fn seen(&self, position: &Position) -> usize {
-        let sign = position.side_to_move().sign() as usize;
+        let sign = position.side_to_move() as usize;
         let (hashes, barriers) = (&self.hashes[sign], &self.barriers[sign]);
         let first_idx = *barriers.last().unwrap_or(&0);
         let hash = position.hash();
@@ -43,7 +43,7 @@ impl GameHistory {
     }
 
     pub fn push(&mut self, position: &Position, reversible: bool) {
-        let sign = position.side_to_move().sign() as usize;
+        let sign = position.side_to_move() as usize;
         let (hashes, barriers) = (&mut self.hashes[sign], &mut self.barriers[sign]);
         if !reversible || hashes.is_empty() {
             barriers.push(hashes.len());
@@ -53,11 +53,40 @@ impl GameHistory {
     }
 
     pub fn pop(&mut self) {
-        let sign = self.side_to_move.sign() as usize;
+        let sign = self.side_to_move as usize;
         let (hashes, barriers) = (&mut self.hashes[sign], &mut self.barriers[sign]);
         if barriers.last().unwrap() + 1 == hashes.len() {
             barriers.pop();
         }
         hashes.pop();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use crate::{
+        position::{Position, fen::Fen},
+        search::game_history::GameHistory,
+    };
+
+    #[rstest]
+    #[case("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "e2e4", true)]
+    #[case("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "e2e4 d7d5", true)]
+    #[case("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "e2e4 d7d5 g1f3", true)]
+    #[case("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "e2e5 d7d5 g1f3", false)]
+    #[case("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", "e2e4 d7d5 g1f4", false)]
+    #[case("rn1q1rk1/pp1bbppp/2p1pn2/8/2QP4/2N2NP1/PP2PPBP/R1B2RK1 b - -", "d8b6 e2e4", true)]
+    #[case("rn1q1rk1/pp1bbppp/2p1pn2/8/2QP4/2N2NP1/PP2PPBP/R1B2RK1 b - -", "d8b6 e2e5", false)]
+    #[case("rn1q1rk1/pp1bbppp/2p1pn2/8/2QP4/2N2NP1/PP2PPBP/R1B2RK1 b - -", "d8b6 e2e5 f8e8", false)]
+    fn from_moves(#[case] fen: Fen, #[case] moves: &str, #[case] valid: bool) {
+        let moves = moves.split_whitespace().collect::<Vec<_>>();
+        let position = Position::try_from(fen).unwrap();
+        let history = GameHistory::from_moves(&position, &moves);
+        assert_eq!(history.is_some(), valid);
+        if let Some((history, _)) = history {
+            assert_eq!(history.hashes[0].len() + history.hashes[1].len(), moves.len() + 1);
+        }
     }
 }
