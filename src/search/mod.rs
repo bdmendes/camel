936ff2dcfc942a510f5fd99f885e46955788c088
Bruno Mendes <bd_mendes@outlook.com>
2026-02-01 12:20:16 +0000

    Add intermediate tactics

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -268,6 +268,23 @@ mod tests {
     #[case("8/5pp1/6p1/5P1P/8/K7/8/k7 w - - 0 1", "f5f6 g7f6 h5h6", 6)]
     #[case("1k6/6R1/3K4/8/8/8/8/8 w - - 6 4", "d6c6 b8a8 c6b6 a8b8 g7g8", 5)]
     #[case("2k5/4B3/1K6/8/4B3/8/8/8 w - - 12 7", "e4f5 c8b8 e7d6 b8a8 f5e4", 3)]
+    #[case("8/1p1b1k1r/p1pB2q1/2Pp3p/3PpPpQ/2P3P1/P5K1/4R3 w - - 0 45", "h4e7 f7g8 e7f8", 2)]
+    #[case("1r4k1/2RQ1rp1/4p2p/pp1p4/8/7P/5PPK/4q3 w - - 0 38", "d7f7 g8h7 f7g7", 2)]
+    #[case("2Q5/p3qkpp/5pb1/8/1p1R4/5P1P/PP4P1/7K b - - 0 35", "e7e1 h1h2 e1e5", 2)]
+    #[case("5r1k/6pp/5q2/8/1Q6/8/6PP/2R3K1 b - - 2 46", "f6f2 g1h1 f2f1 c1f1 f8f1", 3)]
+    #[case("r4rk1/pp2pp1p/6p1/1N2q3/2nN2Q1/8/P4PPP/4RK1R b - - 3 18", "c4d2 f1g1 e5e1", 2)]
+    #[case("1rbk4/1p1p1Qpp/p2R4/P7/1P2r3/2q3P1/5K1P/3R4 w - - 0 28", "d6d7 c8d7 f7d7", 2)]
+    #[case("7k/pp4p1/2p4p/6q1/8/2N1r2P/PPPQ2P1/R6K b - - 2 26", "e3h3 g2h3 g5d2", 2)]
+    #[case("3r1k2/pp3p2/3pqp2/5N1p/2PP1Q2/6P1/P4P1P/6K1 w - - 5 32", "f4h6 f8e8 f5g7", 2)]
+    #[case("2kr4/1pp2B2/p2p1R2/4p2p/4P3/3P3q/PPP1KBr1/3R4 w - - 1 26", "f7e6 h3e6 f6e6", 2)]
+    #[case("r1bk1bNr/1pq2Qpp/2np4/4p3/2p1P3/8/PP3PPP/R4K1R w - - 4 17", "f7f8 d8d7 f8g7", 2)]
+    #[case("6k1/1pRbb2p/4p1pP/p2p4/4qBP1/P5Q1/1P3K2/8 b - - 3 26", "e7h4 g3h4 e4f4", 3)]
+    #[case("2q3k1/6pp/r2PR3/1p1n1p2/5B2/2P2B2/5PPP/4R1K1 b - - 0 38", "c8e6", 3)]
+    #[case("5r2/kpb1q2p/p1R3pP/3Pp1P1/3pPp2/1Q1P3B/PP6/1K6 w - - 8 35", "d5d6 c7d6 b3b6 a7b8 c6d6", 4)]
+    #[case("r5rk/1p1nq2p/3pnp1p/1pp5/4PP1P/P2PQ3/BPP2PR1/2K3R1 w - -", "g2g8 a8g8 g1g8 h8g8 f4f5", 4)]
+    #[case("r5k1/2qn1pp1/bpp2n1p/p2pB3/8/2PQ3P/PPB1NPP1/R4RK1 b - - 0 18", "c7e5", 2)]
+    #[case("8/p4pp1/1pp4p/2Pp4/1P1K1k2/P4P1P/8/8 w - - 0 34", "c5b6 a7b6 a3a4", 3)]
+    #[case("8/8/3p1p2/p2PpP2/1p2P1rk/2P5/PP2B1K1/8 w - - 0 42", "e2g4 h4g4 c3c4", 9)] // what's up here?
     fn pvs_tactics(#[case] fen: Fen, #[case] moves: &str, #[case] depth: Depth) {
         with_searcher(10_000, |searcher| {
             let position = Position::try_from(fen.clone()).unwrap();
