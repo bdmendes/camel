e0abdfe938ca0bffabd4f64d3480c0283b353202
Bruno Mendes <bd_mendes@outlook.com>
2026-04-04 18:24:11 +0100

    Test Score::from

--- src/evaluation/score.rs
+++ src/evaluation/score.rs
@@ -4,6 +4,7 @@ pub type ValueScore = i16;
 
 pub const MATE_SCORE: ValueScore = ValueScore::MIN + 2;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Score {
     Value(ValueScore),
     Mate(u8),
@@ -32,5 +33,29 @@ impl Display for Score {
 impl Score {
     pub fn is_mate(value: ValueScore) -> bool {
         value.abs() >= (MATE_SCORE + u8::MAX as i16).abs()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(MATE_SCORE, Score::Mate(0))]
+    #[case(MATE_SCORE + 1, Score::Mate(1))]
+    #[case(MATE_SCORE + 2, Score::Mate(1))]
+    #[case(MATE_SCORE + 3, Score::Mate(2))]
+    #[case(-MATE_SCORE, Score::Mate(0))]
+    #[case(-MATE_SCORE - 1, Score::Mate(1))]
+    #[case(-MATE_SCORE - 2, Score::Mate(1))]
+    #[case(-MATE_SCORE - 3, Score::Mate(2))]
+    #[case(0, Score::Value(0))]
+    #[case(1200, Score::Value(1200))]
+    #[case(-1200, Score::Value(-1200))]
+    fn parse(#[case] value: ValueScore, #[case] expected: Score) {
+        let score = Score::from(value);
+        assert_eq!(score, expected);
+        assert_eq!(Score::is_mate(value), matches!(score, Score::Mate(_)))
     }
 }
